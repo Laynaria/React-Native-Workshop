0d21e1a4d0d6826d8e1f122d37adffbb9f3dd6af
Laynaria <pro.laynaria@outlook.fr>
2024-06-11 16:01:10 +0200

    changed the app icon

--- app/index.tsx
+++ app/index.tsx
@@ -134,7 +134,7 @@ export default function Index() {
         <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
       </EmojiPicker>
 
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
     </GestureHandlerRootView>
   );
 }
