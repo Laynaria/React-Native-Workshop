e3977a9e6f7a1ae8e9fa38e9937b9b2bf312523e
Laynaria <pro.laynaria@outlook.fr>
2024-06-11 11:45:12 +0200

    user can now select an img, crop it, etc, and app picture gets then updated on app

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,40 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View, StyleSheet, Image } from "react-native";
+import { View, StyleSheet } from "react-native";
+import * as ImagePicker from "expo-image-picker";
 
 import ImageViewer from "@/components/ImageViewer";
 import Button from "@/components/Button";
+import { useState } from "react";
 
 const PlaceholderImage = require("../assets/images/background-image.png");
 
 export default function Index() {
+  const [selectedImg, setSelectedImg] = useState<string | null>(null);
+
+  const pickImgAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImg(result.assets[0].uri);
+    } else {
+      alert("You didn't pick any pic!");
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceholderImage} />
+        <ImageViewer
+          placeholderImageSource={PlaceholderImage}
+          selectedImg={selectedImg}
+        />
       </View>
 
       <View style={styles.footerContainer}>
-        <Button label="Choose a photo" theme="primary" />
+        <Button label="Choose a photo" theme="primary" onPress={pickImgAsync} />
         <Button label="Use this photo" />
       </View>
       <StatusBar style="auto" />
--- components/ImageViewer.tsx
+++ components/ImageViewer.tsx
@@ -1,7 +1,11 @@
 import { StyleSheet, Image } from "react-native";
 
-export default function ImageViewer({ placeholderImageSource }: any) {
-  return <Image source={placeholderImageSource} style={styles.image} />;
+export default function ImageViewer({
+  placeholderImageSource,
+  selectedImg,
+}: any) {
+  const imgSource = selectedImg ? { uri: selectedImg } : placeholderImageSource;
+  return <Image source={imgSource} style={styles.image} />;
 }
 
 const styles = StyleSheet.create({
